34ac7a08aca2ea2d1f5a76f6280392b3680531e6
Brad Reimer <breimer@dccnet.com>
2017-05-13 23:53:32 -0700

    Add placeholder commands.
    * Added a command binding collection to the main view model.

--- UmlDiagrams/SequenceViz/MainWindowViewModel.cs
+++ UmlDiagrams/SequenceViz/MainWindowViewModel.cs
@@ -1,9 +1,42 @@
-﻿namespace SequenceViz
+﻿using System;
+using System.Windows.Input;
+
+namespace SequenceViz
 {
 	class MainWindowViewModel : ObservableObject
 	{
+		#region Members
+
+		private readonly CommandBindingCollection m_commandBindings;
 		private SequenceThemes m_sequenceTheme;
 		private string m_sequenceText;
+
+		#endregion
+
+		#region Constructor
+
+		public MainWindowViewModel()
+		{
+			m_commandBindings = new CommandBindingCollection();
+			InitializeCommandBindings();
+		}
+
+		private void InitializeCommandBindings()
+		{
+			m_commandBindings.Add(new CommandBinding(ApplicationCommands.New, NewExecuted));
+			m_commandBindings.Add(new CommandBinding(ApplicationCommands.Open, OpenExecuted));
+			m_commandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveExecuted));
+			m_commandBindings.Add(new CommandBinding(ApplicationCommands.SaveAs, SaveAsExecuted));
+		}
+
+		#endregion
+
+		#region Properties
+
+		public CommandBindingCollection CommandBindings
+		{
+			get { return m_commandBindings; }
+		}
 
 		public SequenceThemes SequenceTheme
 		{
@@ -16,5 +49,30 @@
 			get { return m_sequenceText; }
 			set { Set(ref m_sequenceText, value); }
 		}
+
+		#endregion
+
+		#region Commands
+
+		private void NewExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+		private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SaveAsExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
 	}
 }
